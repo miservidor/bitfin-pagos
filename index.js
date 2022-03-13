72140a545147a132b651c556116cc316d0664544
Jorge Fernandez <jorgefer2005@gmail.com>
2022-03-13 23:45:47 +0100

    fifth version

--- index.js
+++ index.js
@@ -9,61 +9,8 @@ const stripe = Stripe("sk_test_51KcXglJh5z9qKSwjgd5velSj0D6b760cmdWLun00HGAEKypF
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-app.get('/', function(req, res) {
+app.get('/hello', function(req, res) {
     res.send('hello world 4');
-  });
-app.post("/create-payment-intent", (req, res) => {
-
-  stripe.paymentIntents.create(
-    {
-      amount: parseInt(req.body.amount),
-      currency: "usd",
-      payment_method_types: ["card"],
-    },
-    function (err, paymentIntent) {
-      if (err) {
-        res.status(500).json(err.message);
-      } else {
-          console.log(paymentIntent)
-        res.status(201).json(paymentIntent);
-      }
-    }
-  );
-});
-app.post('/create-checkout-session', async (req, res) => {
-    const { priceId } = req.body;
-  
-    // See https://stripe.com/docs/api/checkout/sessions/create
-    // for additional parameters to pass.
-    try {
-      const session = await stripe.checkout.sessions.create({
-        mode: 'subscription',
-        payment_method_types: ['card'],
-        line_items: [
-          {
-            price: priceId,
-            // For metered billing, do not pass quantity
-            quantity: 1,
-          },
-        ],
-        // {CHECKOUT_SESSION_ID} is a string literal; do not change it!
-        // the actual Session ID is returned in the query parameter when your customer
-        // is redirected to the success page.
-        success_url: 'https://www.webactiva.de',
-        cancel_url: 'https://www.webactiva.de',
-      });
-  
-      res.send({
-        sessionId: session.id,
-      });
-    } catch (e) {
-      res.status(400);
-      return res.send({
-        error: {
-          message: e.message,
-        }
-      });
-    }
   });
 
 app.listen(PORT, () => console.log(`Server ready on port ${PORT}`));
